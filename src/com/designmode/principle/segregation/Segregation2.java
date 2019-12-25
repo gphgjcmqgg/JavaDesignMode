f70902f16452fcd04265b29bfc2b2c3391ebce6c
jinggao <232214002@qq.com>
2019-12-25 10:29:24 +0800

    接口隔离原则

--- src/com/designmode/principle/segregation/Segregation2.java
+++ src/com/designmode/principle/segregation/Segregation2.java
@@ -1,47 +1,45 @@
 package com.designmode.principle.segregation;
 
-public class Segregation1 {
+// 接口隔离原则
+public class Segregation2 {
   public static void main (String[] args) {
-    A a = new A();
-    B b = new B();
+    AA a = new AA();
+    BB b = new BB();
     a.dependence1(b);
     a.dependence4(b);
     a.dependence5(b);
   }
 }
 
-interface IInterface {
+interface IInterfaceA {
   void operation1();
-  void operation2();
-  void operation3();
+}
+
+interface IInterfaceB {
   void operation4();
   void operation5();
 }
-class A {
-  public void dependence1(IInterface operation) {
+
+interface IInterfaceC {
+  void operation2();
+  void operation3();
+}
+
+class AA {
+  public void dependence1(IInterfaceA operation) {
     operation.operation1();
   }
-  public void dependence4(IInterface operation) {
+  public void dependence4(IInterfaceB operation) {
     operation.operation4();
   }
-  public void dependence5(IInterface operation) {
+  public void dependence5(IInterfaceB operation) {
     operation.operation5();
   }
 }
-class B implements IInterface {
+class BB implements IInterfaceA, IInterfaceB {
   @Override
   public void operation1() {
     System.out.println("operation1");
-  }
-
-  @Override
-  public void operation2() {
-    System.out.println("operation2");
-  }
-
-  @Override
-  public void operation3() {
-    System.out.println("operation3");
   }
 
   @Override
